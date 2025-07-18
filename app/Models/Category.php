90c7b867c6d360d2a3ebe0252488c24f47047504
Layth <layth.hikmat20@gmail.com>
2025-07-18 17:13:16 +0300

    add paretId for categories (sunCategories)

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,15 +9,21 @@ class Category extends Model
 {
     use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'nameEN',
         'nameKU',
         'nameAR',
         'image',
+        'parent_id',
     ];
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
 }
