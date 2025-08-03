2cf7bca67caa743dda83684b673cd24344942291
Layth <layth.hikmat20@gmail.com>
2025-08-03 12:51:17 +0300

    add FCM service and controller

---
+++ app/Http/Controllers/FirebaseController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\FirebaseService;
+use Illuminate\Http\Request;
+
+class FirebaseController extends Controller
+{
+    public function sendToDevice(FirebaseService $firebase)
+    {
+        $deviceToken = 'f9qT9uLFQymtfxsrGConqq:APA91bHhGK1RGYbw2jy5hEKwq-wjiKLJzqBf6VIs2-xIZXU4GKimnOPWwkas02PvuhqJw-Bj6ebMh_ww2b7q8FSyW4l3AlyQKvQO6L_s-hdPUzhckrFmLZc';
+        $title = 'New Message';
+        $body = 'You have a new notification.';
+
+        try {
+            $firebase->sendNotification($deviceToken, $title, $body);
+            return response()->json(['message' => 'Notification sent']);
+        } catch (\Throwable $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+}
